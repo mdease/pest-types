7ede23d7d10356e849a302ddc4d1b6fa96bc2a25
Maddie <mdd@mit.edu>
2018-08-03 11:52:33 -0400

    Finished struct instantiation

--- src/main.rs
+++ src/main.rs
@@ -6,36 +6,28 @@ use pest::Parser;
 
 #[derive(Parser)]
 #[grammar = "types.pest"]
-struct TypesParser;
+struct TypesParser{
+  first_field: u16,
+  second_field: u8,
+  third_field: bool
+}
 
 fn main() {
   // macro for defining and filling structs
-  macro_rules! create_struct {
-    ($name: ident, $($i: ident, $e: expr),*) => {{
-      struct $name {
-        $($i: u16),*
-      }
+  // macro_rules! create_struct {
+  //   ($name: ident, $($i: ident, $e: expr),*) => {{
+  //     struct $name {
+  //       $($i: u16),*
+  //     }
 
-      let tmp: $name = $name {
-        $($i: $e),*
-      };
+  //     let tmp: $name = $name {
+  //       $($i: $e),*
+  //     };
 
-      tmp
-    }}
-  }
+  //     tmp
+  //   }}
+  // }
 
-  // bytes for 9 and 5 as u16
-  let pairs = TypesParser::parse(Rule::t, &[9, 0, 5, 0]).unwrap_or_else(|e| panic!("{}", e));
-
-  for pair in pairs {
-    // get the matched u16s
-    let nums = pair.clone().into_span().as_type();
-
-    // create the struct
-    let my_struct = create_struct!(MyNums, number_one, nums[0], number_two, nums[1]);
-
-    // check struct fields
-    println!("MyNums.number_one: {}", my_struct.number_one);
-    println!("MyNums.number_two: {}", my_struct.number_two);
-  }
+  let t = TypesParser::parse_and_create(Rule::t, &[9, 0, 1, 0], true);
+  println!("first_field: {}\nsecond_field: {}\nthird_field: {}", t.first_field, t.second_field, t.third_field);
 }
